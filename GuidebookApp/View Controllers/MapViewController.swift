37a1dfa00008560156db0cf265111273794db0e4
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-11 15:49:28 -0800

    Added the logic for the map view controller by adding the ability to drop a pin based on th places address, and the ability to make a route.

--- GuidebookApp/View Controllers/MapViewController.swift
+++ GuidebookApp/View Controllers/MapViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
     // Properties
     var place:Place?
+    
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +24,65 @@ class MapViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        
+        // Check for a place and plot the pin
+        if place != nil {
+            plotPin(place!)
+        }
+        
     }
-    */
+    
+    func plotPin(_ p:Place) {
+        
+        let pin = MKPointAnnotation()
+        
+        // Get the long and lat from the plcae
+        pin.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(p.lat), longitude: CLLocationDegrees(p.long))
+        
+        // Set the title
+        pin.title = p.name!
+        
+        // Add the pin to the map view
+        mapView.addAnnotation(pin)
+        
+        // Center the map aroud the pin
+        mapView.showAnnotations([pin], animated: true)
+        
+    }
 
+    @IBAction func backTapped(_ sender: UIButton) {
+        
+        // Dismiss the map view controller
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func locateTapped(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+    @IBAction func routeTapped(_ sender: UIButton) {
+        
+        // Male sure there arent nil values
+        guard place != nil && place!.address != nil else {
+            return
+        }
+        
+        // Replace all spaces with +
+        let newAddress = place!.address!.replacingOccurrences(of: " ", with: "+")
+        
+        let url = URL(string: "http://maps.apple.com/?address=" + newAddress)
+        
+        if url == url {
+            
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+            
+        }
+        
+    }
+    
 }
